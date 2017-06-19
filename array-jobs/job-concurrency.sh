ec81ae16d604cc9fcb02eaf03a37e977514ef2d6
Chris Scheller <schelcj@pobox.com>
2017-06-19 13:46:42 -0400

    include examples of controlling job steps and concurrency

---
+++ array-jobs/job-concurrency.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=uniqname@umich.edu
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2000
+#SBATCH --array=1-100%10
+
+R ./script.R input_file_${SLURM_ARRAY_TASK_ID}.txt
